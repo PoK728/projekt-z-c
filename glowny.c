e80724fbcdeb3ee06ef695cfe12fae4dbff5481e
PoK728 <siaziawiktor@gmail.com>
2026-01-06 17:38:23 +0100

    troche rejestracji

--- glowny.c
+++ glowny.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+typedef enum { miesozerny,roslinozerny,wszystkozerny} Dieta;
+typedef enum { spokojny,agresywny,impulsywny,nieprzewidywalny,przewidywalny} Temperament;
+typedef enum { bezpieczny, pod_obserwacja, zagrozenie, ucieczka,awaryjna_kwarantanna} Status_bezpieczenstwa;
+
 struct Dinozaur{
     char gatunek[100];
-    char dieta[20];
+    Dieta dieta;
     float masa;
     int zagroda;
-    char temperament[20];
-    char status_bezpieczenstwa[20];
+    Temperament temperament;
+    Status_bezpieczenstwa status_bezpieczenstwa;
 }; //zestaw cech obiektu 
 
 
 int main(){
     int zmienna;
+    char *gat = (char*)malloc(sizeof(char));
+    int *diet = (int*)malloc(sizeof(int));
+    float *m = (float*)malloc(sizeof(float));
+    int *zag = (int*)malloc(sizeof(int));
+    int *tem = (int*)malloc(sizeof(int));
+    int *sb = (int*)malloc(sizeof(int));
     while(1){
         printf("==Witaj w systemie zarzadzania parkiem dinozaurow==\n ");
         printf("Wybierz opcje: \n");
@@ -25,9 +35,33 @@ int main(){
         scanf("%d",&zmienna);
         switch(zmienna){
             default:
-                printf("Wybierz z 0-6!");
+                printf("Wybierz z 0-6!\n");
             case 1:
-                printf("REJESTRACJA");
+                printf("==REJESTRACJA==\n");
+                while(1){
+                printf("Podaj nazwe gatunku: \n");
+                fgets("%s",&gat);
+                int wybordiety;
+                printf("Wybierz diete: \n");
+                printf("1. miesozerny: \n");
+                printf("2. roslinozerny: \n");
+                printf("3. wszystkozerny: \n");
+                scanf("%d",&wybordiety);
+                switch(wybordiety){
+                    default:
+                        printf("Wybierz z 1-3!\n");
+                    case 1:
+                        printf("Wybrano miesozerny!\n");
+                    case 2:
+                        printf("Wybrano roslinozerny!\n");
+                    case 3:
+                        printf("Wybrano wszystkozerny!\n");                        
+                }
+                diet = wybordiety;
+                printf("Wprowadz mase: \n");
+                scanf("%f",&m);
+
+                }
                 break;
             case 2:
 
